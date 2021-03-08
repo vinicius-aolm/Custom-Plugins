a6fa111d3e4f65160ddbf64c257d00fa2ced05d0
dispelundead <ashenzar@protonmail.com>
2021-03-08 08:44:37 -0600

    fix

--- plugins/firstkill.py
+++ plugins/firstkill.py
@@ -31,7 +31,7 @@ async def firstkill(message: Message):
 async def auto_fk(message: Message):
     global AFK
     lines = message.text
-    lines_count = len(lines.split("\n\n")[0]) - 1
+    lines_count = len(lines.split("\n\n")[0].split("\n")) - 1
     info = await message.reply("Obtendo FK.")
     try:
         deads = await build_list(lines)
