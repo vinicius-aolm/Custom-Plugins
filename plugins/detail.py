34381a04391d356a501acc8491036607996bf8b6
Layer <ashenzar@protonmail.com>
2021-03-08 18:37:36 -0600

    typo

--- plugins/detail.py
+++ plugins/detail.py
@@ -9,4 +9,4 @@ from userge import Message, userge
     },
 )
 async def detail_hi(message: Message):
-  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dik Size :** {random.choice(range(0,10))} centimeter\n**3. Lie Meter Reading :** {random.choice(range(0,100))}% \n**4. Gey Count :** {random.choice(range(0,100))}%")
+  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dick Size :** {random.choice(range(0,10))} centimeter\n**3. Lie Meter Reading :** {random.choice(range(0,100))}% \n**4. Gay Count :** {random.choice(range(0,100))}%")
