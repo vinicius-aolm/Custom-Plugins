047b4e7b337c13f58f3b04beb7de751bef16c543
vinicius-aolm <75711498+vinicius-aolm@users.noreply.github.com>
2023-06-18 17:48:25 -0300

    detail.py

--- plugins/detail.py
+++ plugins/detail.py
@@ -1,12 +1,1 @@
-import asyncio
-import random
-from userge import Message, userge
-@userge.on_cmd(
-    "detail",
-    about={
-        "header": "detect details of user. Ignore measurement if you are Afrikan",
-        "usage": "{tr}reply to detect details. Dont reply any Afrikans",
-    },
-)
-async def detail_hi(message: Message):
-  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dick Size :** {random.choice(range(0,10))} centimeter\n**3. Lie Meter Reading :** {random.choice(range(0,100))}% \n**4. Gay Count :** {random.choice(range(0,100))}%")
+
